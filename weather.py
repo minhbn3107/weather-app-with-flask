ec572ad0f4a64798d57e676d7a3a6dc809ad90fd
bnminh <minhbn3107>
2024-01-07 09:31:30 +0700

    replace all " with '

--- weather.py
+++ weather.py
@@ -6,24 +6,25 @@ import requests
 load_dotenv()
 
 
-def get_current_weather(city="Ho Chi Minh City"):
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv('API_KEY')}&q={city}&units=metric"
+def get_current_weather(city='Ho Chi Minh City'):
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={
+        os.getenv('API_KEY')}&q={city}&units=metric'
 
     weather_data = requests.get(request_url).json()
 
     return weather_data
 
 
-if __name__ == "__main__":
-    print("Get Current Weather Conditions".center(80, "*"))
+if __name__ == '__main__':
+    print('Get Current Weather Conditions'.center(80, '*'))
 
-    city = input("Please enter a city name:\n")
+    city = input('Please enter a city name:\n')
 
     # Check for empty strings or string with only spaces
     if not bool(city.strip()):
-        city = "Ho Chi Minh City"
+        city = 'Ho Chi Minh City'
 
     weather_data = get_current_weather(city)
 
-    print("\n")
+    print('\n')
     pprint(weather_data)
